063dba81f3412f11d60f8e055ee8c2474ca52667
BenTheDan <76922502+BenTheDan@users.noreply.github.com>
2026-02-18 09:06:59 +0100

    v1.2.9

--- IngameRank/version.h
+++ IngameRank/version.h
@@ -1,8 +1,8 @@
 #pragma once
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 2
-#define VERSION_PATCH 6
-#define VERSION_BUILD 113
+#define VERSION_PATCH 9
+#define VERSION_BUILD 119
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
